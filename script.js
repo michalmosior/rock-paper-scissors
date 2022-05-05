f78a14927806d9197624a8362df80c9ff32174a3
michalmosior <michal.mosior@gmail.com>
2022-05-05 19:30:36 +0200

    modified:   css/main.css 	modified:   css/main.css.map 	modified:   index.html 	modified:   sass/main.scss 	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const rulesBtn = document.querySelector('.rules-btn');
 const rulesContainer = document.querySelector('.rules');
+const optionBtns = document.querySelectorAll('.option-container__btn');
+const optionContainer = document.querySelector('.option-container');
+
+const startGame = () => {
+	optionContainer.classList.add('started');
+};
 
 const showRules = () => {
 	const closeBtn = document.querySelector('.rules__close-btn');
@@ -10,3 +16,6 @@ const showRules = () => {
 };
 
 rulesBtn.addEventListener('click', showRules);
+optionBtns.forEach((el) => {
+	el.addEventListener('click', startGame);
+});
