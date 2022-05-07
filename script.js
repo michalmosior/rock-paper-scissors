d68d6c32e810a14fe2228e13aa26ad0bae12289c
michalmosior <michal.mosior@gmail.com>
2022-05-07 17:09:17 +0200

    modified:   css/main.css 	modified:   css/main.css.map 	modified:   index.html 	modified:   sass/main.scss 	modified:   script.js

--- script.js
+++ script.js
@@ -2,9 +2,53 @@ const rulesBtn = document.querySelector('.rules-btn');
 const rulesContainer = document.querySelector('.rules');
 const optionBtns = document.querySelectorAll('.option-container__btn');
 const optionContainer = document.querySelector('.option-container');
+const gameButtons = document.querySelectorAll('.button');
+const weaponChoice = document.querySelector('.weapon-choice');
+const gameField = document.querySelector('.game-field');
+let playerOneChoice;
 
-const startGame = () => {
+const chooseEnemy = (e) => {
 	optionContainer.classList.add('started');
+	const human = document.querySelector('.human');
+	const cpu = document.querySelector('.cpu');
+	e.target.classList.add('choosen-option');
+	if (e.target.matches('span')) {
+		e.target.parentElement.classList.add('choosen-option');
+	}
+	if (human.classList.contains('choosen-option')) {
+		human.classList.add('right');
+		cpu.classList.add('none');
+	} else if (cpu.classList.contains('choosen-option')) {
+		cpu.classList.add('left');
+		human.classList.add('none-left');
+		vsCpu();
+	}
+};
+
+const weaponSelection = () => {
+	const weapon = (e) => {
+		console.log(e.target);
+		if (e.target === gameButtons[0]) {
+			weaponChoice.innerHTML = `<div class="paper small">
+			<button class="paper__btn button"></button>
+		</div>`;
+		} else if (e.target === gameButtons[1]) {
+			weaponChoice.innerHTML = `<div class="scissors small">
+			<button class="scissors__btn button"></button>
+		</div>`;
+		} else if (e.target === gameButtons[2]) {
+			weaponChoice.innerHTML = `<div class="rock small">
+			<button class="rock__btn button"></button>
+		</div>`;
+		}
+	};
+	gameButtons.forEach((el) => {
+		el.addEventListener('click', weapon);
+	});
+};
+
+const vsCpu = () => {
+	weaponSelection();
 };
 
 const showRules = () => {
@@ -17,5 +61,5 @@ const showRules = () => {
 
 rulesBtn.addEventListener('click', showRules);
 optionBtns.forEach((el) => {
-	el.addEventListener('click', startGame);
+	el.addEventListener('click', chooseEnemy);
 });
